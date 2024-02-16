d4d5546a9c3dd5f3b554a2793a36fb91d60f1bb9
edavidson1410 <129311010+edavidson1410@users.noreply.github.com>
2024-02-16 17:14:20 -0600

    added profile image

--- portfolio/src/app/components/HeroSection.jsx
+++ portfolio/src/app/components/HeroSection.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const HeroSection = () => {
+
   return (
-    <section className="m-20">
-      <div className="flex ">
+    <section className="flex m-20">
+      <div>
         <h1 className="lg:text-6xl">Hi! I'm Eric Davidson</h1>
         <p>This is the description of me</p>
+      </div>   
+      <div>
+        <img className="border-solid rounded-full" src="/assets/placeholder.jpg" alt="profile picture" />
       </div>
 
     </section>
