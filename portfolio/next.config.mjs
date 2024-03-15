cfc580d113079a6220926fc332bbd66d6971d833
edavidson1410 <129311010+edavidson1410@users.noreply.github.com>
2024-03-14 20:16:56 -0500

    next.config changes

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        RESEND_API_KEY : "re_hFyTJsDG_NoW9s35EouXkWMmpJo3mWNHR"
-    }
+
 };
 
 export default nextConfig;
