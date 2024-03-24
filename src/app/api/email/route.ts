d7166baba971870c0009991104b9a1f0dc8fe924
edavidson1410 <129311010+edavidson1410@users.noreply.github.com>
2024-03-24 17:44:51 -0500

    Contact form up and running

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,11 +2,14 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
 export async function POST(request: Request, name: string, email: string, message: string, date: string) {
+
   try {
-    const result =
-      await sql`INSERT INTO contact (Name, Email, Message, Date) VALUES (${name}, ${email}, ${message}, ${date}`;
-    return NextResponse.json({ result }, { status: 200 });
+    const info = await request.json();
+
+      await sql`INSERT INTO contact (Name, Email, Message, Date) VALUES (${info.name}, ${info.email}, ${info.message}, ${info.date})`;
+    return NextResponse.json({ info }, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -8,15 +8,32 @@ function ContactForm(props) {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [message, setMessage] = useState();
-  const [date, setDate] = useState();
 
-  const url = 'http://localhost:3000/email';
+  const url = 'http://localhost:3000/api/email';
+  const date = new Date();
 
-  const onSubmit = (e) => {
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+  const handleMessageChange = (event) => {
+    const value = event.target.value;
+    setMessage(value);
+  };
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log('hi')
-    fetch(url, {
+    await fetch(url, {
       method: 'POST',
+      body: JSON.stringify({name: name, email: email, message: message, date: date }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
     })
     .then(response => {
       console.log(response);
@@ -32,14 +49,14 @@ function ContactForm(props) {
         <div className="mb-4">
             <div className="mx-0 mb-1 sm:mb-4">
                 <div className="mx-0 mb-1 sm:mb-4">
-                    <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="name" autoComplete="given-name" placeholder="Your name" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="name"/>
+                    <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label><input onChange={handleNameChange} type="text" id="name" autoComplete="given-name" placeholder="Your name" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="name"/>
                 </div>
                 <div className="mx-0 mb-1 sm:mb-4">
-                    <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider"></label><input type="email" id="email" autoComplete="email" placeholder="Your email address" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="email"/>
+                    <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider"></label><input onChange={handleEmailChange} type="email" id="email" autoComplete="email" placeholder="Your email address" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="email"/>
                 </div>
             </div>
             <div className="mx-0 mb-1 sm:mb-4">
-                <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label><textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
+                <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label><textarea onChange={handleMessageChange} id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
             </div>
         </div>
           <div className="text-center">
